60a353b15d1730c5c99bb4caaf4c38c9323feb04
seth <sadams65@uncc.edu>
2022-03-24 15:39:48 -0400

    fix2

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,13 @@ export default function Home() {
 			<div className="big title accent">Behind the scenes:</div>
 				<div>(Backend work + front end work display here)</div>
 				<div className="big title accent">Meet the team:</div>
+				<img src="./TeamPhotos/Murtadha_Marzouq.png" alt="Abu" width="150" height="150"></img>
 				<div>Murtadha Marzouq (Abu) is an IT specialist with over 8 years of experience in IT infrastructure and networking.  
 				Murtadha graduated from Central Piedmont College and is currently a student at The University of North Carolina in Charlotte. 
 				Murtadha provides IT support for our clients in Charlotte, NC. He is experienced in setting up IT infrastructure from Cisco networking to  Active Directory.  
 				He also has experience in Linux and setting up Linux-based services such as Secure FTP, MySQL and Apache web server. 
 				Murtadha was born in Damascus, Syria and has been in the United States for 10 years.</div>
-				<img src="./TeamPhotos/Murtadha_Marzouq.png" alt="Abu" width="500" height="333"></img>
+				
 			</div>
 			<style jsx>
 				{`
