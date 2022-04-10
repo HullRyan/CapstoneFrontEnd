0378489f32d4c5d5ffe961ea6a3c5034966c272e
seth <sadams65@uncc.edu>
2022-04-09 20:49:56 -0400

    Create back end page

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,6 +19,9 @@ export default function Navbar() {
 				<Link href="/charts">
 					<a className="header-item">Charts</a>
 				</Link>
+				<Link href="/document">
+					<a className="header-item">Back-End</a>
+				</Link>
 				<span className="header-item right">
 					<ToggleTheme />
 				</span>
---
+++ pages/document.js
@@ -0,0 +1,15 @@
+/**
+ * @author Ryan Hull <hull.ryanc@gmail.com>
+ * @author Seth Adams <sadams65@uncc.edu>
+ */
+
+ import Head from "next/head";
+ import Scroll from "../components/ScrollText";
+ import LoadingScreen from "../components/LoadingScreen";
+ import Image from "next/image";
+
+ export default function Home(){
+     return(
+        <div>Test</div>
+     );
+ }
