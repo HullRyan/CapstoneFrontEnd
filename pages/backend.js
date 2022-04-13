c3f8e59862fe954cf58fd28c8146b9a9008f1096
Ryan Hull <hull.ryanc@gmail.com>
2022-04-13 12:36:52 -0400

    Changed to pdf

--- pages/backend.js
+++ pages/backend.js
@@ -4,138 +4,53 @@
  * @author Seth Adams <sadams65@uncc.edu>
  */
 
- import Image from "next/image";
+import Image from "next/image";
 
- export default function Backend(){
-     return(
-        <div>
-            <div className="backend-container">
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_1.jpg"
-                    alt="Page1"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_2.jpg"
-                    alt="Page2"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_3.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_4.jpg"
-                    alt="Page4"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_5.jpg"
-                    alt="Page5"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_6.jpg"
-                    alt="Page6"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_7.jpg"
-                    alt="Page7"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_8.jpg"
-                    alt="Page8"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_9.jpg"
-                    alt="Page9"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_10.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_11.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_12.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_13.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_14.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_15.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_16.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_17.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_18.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_19.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_20.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_21.jpg"
-                    alt="Page3"
-                ></img>
-                <img
-                    src="/DocumentPhotos/Project Gemini Report-1 (1)1024_22.jpg"
-                    alt="Page3"
-                ></img>
+export default function Backend() {
+	return (
+		<div>
+			<div className="backend-container">
+				<iframe
+					src="https://drive.google.com/file/d/1036be-M_JZcKqpo__k-AeRSHPlGvPP7N/preview"
+					width="100%"
+					height="100%"
+					allow="autoplay"
+				></iframe>
+			</div>
 
-                <div>
-					<p>This was written by <u>Miguel Morel</u> & <u>Willis Reid</u></p>
-				</div>
-                
-            </div>
+			<div className="text">
+				<p>
+					*Written by <u>Miguel Morel</u> & <u>Willis Reid</u>
+				</p>
+			</div>
 
-            <div className="text">
-				<p>*Written by <u>Miguel Morel</u> & <u>Willis Reid</u></p>
-		    </div>
-
-            <style jsx>
-					{`
-						.backend-container {
-                            height: 80%;
-                            max-height: 1024px;
-                            width: 80%;
-                            max-width: 791px;
-                            overflow: auto;
-                            display: block;
-                            margin-left: auto;
-                            margin-right: auto;
-                            border: 8px solid black;
-                            border-radius: 5px;
-                            aspect-ratio: 791 / 1024;
-                        }
-                        .backend-container img {
-                            width: 100%;
-                            object-fit: contain;
-                        }
-                        .text {
-                            display: flex;
-                            justify-content: center;
-                            align-items: center;
-                            justify-content: center;
-                        }
-					`}
-				</style>
-        </div>         
-     );
- }
+			<style jsx>
+				{`
+					.backend-container {
+						height: 80%;
+						max-height: 1024px;
+						width: 80%;
+						max-width: 791px;
+						overflow: none;
+						display: block;
+						margin-left: auto;
+						margin-right: auto;
+						border: 8px solid black;
+						border-radius: 5px;
+						aspect-ratio: 791 / 1024;
+					}
+					.backend-container img {
+						width: 100%;
+						object-fit: contain;
+					}
+					.text {
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						justify-content: center;
+					}
+				`}
+			</style>
+		</div>
+	);
+}
